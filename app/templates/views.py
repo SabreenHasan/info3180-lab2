f76f8dc7ccd24e73e7abf2ba9a73c9d1f7407a85
Sabreen Hasan <sabreen_hasan@yahoo.com>
2020-02-07 11:48:08 -0500

    Adding lab2 folder

---
+++ app/templates/views.py
@@ -0,0 +1,24 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+app.run(debug=True, host="0.0.0.0", port=8080)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
